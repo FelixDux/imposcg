81d8b7d0805d791d6209486bfb5998a2935e1810
FELIX DUX <50945610+FelixDux@users.noreply.github.com>
2021-05-06 14:02:52 +0100

    iteration data e2e test

--- impact-oscillator_test.go
+++ impact-oscillator_test.go
@@ -1,10 +1,11 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
-    "net/http/httptest"
-    "testing"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"testing"
 )
 func TestSwaggerRoute(t *testing.T) {
 
@@ -33,4 +34,35 @@ func TestSwaggerRoute(t *testing.T) {
     if val[0] != "text/html; charset=utf-8" {
         t.Fatalf("Expected \"text/html; charset=utf-8\", got %s", val[0])
     }
+}
+
+func TestIterationDataRoute(t *testing.T) {
+
+    ts := httptest.NewServer(setupServer())
+    // Shut down the server and block until all requests have gone through
+    defer ts.Close()
+
+	resp, err := http.PostForm(fmt.Sprintf("%s/api/iteration/data", ts.URL), 
+        url.Values{"frequency": {"2.8"}, "offset": {"0"}, "r": {"0.8"}, "maxPeriods": {"100"}, "phi": {"0"}, "v": {"0"}, "numIterations": {"100"}})
+
+    if err != nil {
+        t.Fatalf("Expected no error, got %v", err)
+    }
+
+    if resp.StatusCode != 200 {
+        t.Fatalf("Expected status code 200, got %v", resp.StatusCode)
+    }
+
+    val, ok := resp.Header["Content-Type"]
+
+    // Assert that the "content-type" header is actually set
+    if !ok {
+        t.Fatalf("Expected Content-Type header to be set")
+    }
+
+    // Assert that it was set as expected
+    if val[0] != "application/json; charset=utf-8" {
+        t.Fatalf("Expected \"application/json; charset=utf-8\", got %s", val[0])
+    }
+
 }

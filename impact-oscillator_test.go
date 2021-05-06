8650d41795ab9d5de2d93b7b1481c42a5fa4c502
FELIX DUX <50945610+FelixDux@users.noreply.github.com>
2021-05-06 16:03:37 +0100

    iteration image e2e test

--- impact-oscillator_test.go
+++ impact-oscillator_test.go
@@ -36,33 +36,43 @@ func TestSwaggerRoute(t *testing.T) {
     }
 }
 
-func TestIterationDataRoute(t *testing.T) {
+func TestIterationRoutes(t *testing.T) {
 
     ts := httptest.NewServer(setupServer())
     // Shut down the server and block until all requests have gone through
     defer ts.Close()
 
-	resp, err := http.PostForm(fmt.Sprintf("%s/api/iteration/data", ts.URL), 
-        url.Values{"frequency": {"2.8"}, "offset": {"0"}, "r": {"0.8"}, "maxPeriods": {"100"}, "phi": {"0"}, "v": {"0"}, "numIterations": {"100"}})
-
-    if err != nil {
-        t.Fatalf("Expected no error, got %v", err)
+    IterationTests := [] struct {
+        endpoint string
+        contentType string
+    }{
+        {"data", "application/json; charset=utf-8"},
+        {"image", "image/png"},
     }
 
-    if resp.StatusCode != 200 {
-        t.Fatalf("Expected status code 200, got %v", resp.StatusCode)
-    }
+	for _, data := range IterationTests {
+            resp, err := http.PostForm(fmt.Sprintf("%s/api/iteration/%s", ts.URL, data.endpoint), 
+            url.Values{"frequency": {"2.8"}, "offset": {"0"}, "r": {"0.8"}, "maxPeriods": {"100"}, "phi": {"0"}, "v": {"0"}, "numIterations": {"100"}})
 
-    val, ok := resp.Header["Content-Type"]
+        if err != nil {
+            t.Fatalf("Expected no error, got %v", err)
+        }
 
-    // Assert that the "content-type" header is actually set
-    if !ok {
-        t.Fatalf("Expected Content-Type header to be set")
-    }
+        if resp.StatusCode != 200 {
+            t.Fatalf("Expected status code 200, got %v", resp.StatusCode)
+        }
 
-    // Assert that it was set as expected
-    if val[0] != "application/json; charset=utf-8" {
-        t.Fatalf("Expected \"application/json; charset=utf-8\", got %s", val[0])
+        val, ok := resp.Header["Content-Type"]
+
+        // Assert that the "content-type" header is actually set
+        if !ok {
+            t.Fatalf("Expected Content-Type header to be set")
+        }
+
+        // Assert that it was set as expected
+        if val[0] != data.contentType {
+            t.Fatalf("Expected \"%s\", got %s", data.contentType, val[0])
+        }
     }
 
 }

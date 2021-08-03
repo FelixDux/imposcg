29b5303744d8b35687b4faabdf9278a76e947882
FELIX DUX <50945610+FelixDux@users.noreply.github.com>
2021-08-03 10:25:07 +0100

    Fixed unit tests for property info

--- static/js/conponents.test.js
+++ static/js/conponents.test.js
@@ -1,4 +1,4 @@
-import {FullPathBuilder, Parameter, Header, PathsHolder, ParameterSymbols} from './components.js';
+import {FullPathBuilder, Parameter, Header, PathsHolder, ParameterInfo} from './components.js';
 
 const consoleSpy = jest.spyOn(console, 'log').mockImplementation();
 
@@ -6,7 +6,7 @@ function mockSymbolsGetter(data) {
     return (callback) => {callback(data)};
 }
 
-const goodSymbolData = {"Symbols":[{"Parameter":"offset","Symbol":"σ"},{"Parameter":"phi","Symbol":"φ"},{"Parameter":"frequency","Symbol":"ω"}]};
+const goodSymbolData = {"Symbols":[{"Parameter":"offset","Property":"σ"},{"Parameter":"phi","Property":"φ"},{"Parameter":"frequency","Property":"ω"}]};
 
 const goodSymbolsGetter = mockSymbolsGetter(goodSymbolData);
 const badSymbolsGetter = mockSymbolsGetter({});
@@ -18,13 +18,13 @@ describe('Unit tests for looking up symbols for parameter names', () => {
 
     test('Lookup fails gracefully when initialised with badly formatted data', () => {
 
-        const symbols = new ParameterSymbols(badSymbolsGetter);
+        const symbols = new ParameterInfo(badSymbolsGetter);
 
         expect(console.log).toBeCalledTimes(1);
     })
 
     test('Lookup returns correct symbol when available', () => {
-        const symbols = new ParameterSymbols(goodSymbolsGetter);
+        const symbols = new ParameterInfo(goodSymbolsGetter);
 
         expect(symbols.lookup('phi')).toBe('φ');
 
@@ -32,7 +32,7 @@ describe('Unit tests for looking up symbols for parameter names', () => {
     })
 
     test('Lookup returns parameter when no symbol available', () => {
-        const symbols = new ParameterSymbols(goodSymbolsGetter);
+        const symbols = new ParameterInfo(goodSymbolsGetter);
 
         const parameter = 'Beta';
 
@@ -81,7 +81,7 @@ describe('Unit tests for rendering a parameter', () => {
     const badAPIData = JSON.parse('{}');
 
     test('Parameter class fails gracefully when initialised with bad API data', () => {
-        const parameter = new Parameter(badAPIData, new ParameterSymbols(goodSymbolsGetter));
+        const parameter = new Parameter(badAPIData, new ParameterInfo(goodSymbolsGetter), new ParameterInfo(goodSymbolsGetter));
 
         expect(console.log).toBeCalledTimes(1);
 
@@ -91,7 +91,7 @@ describe('Unit tests for rendering a parameter', () => {
     })
 
     test('Parameter class fails gracefully when initialised with bad symbols data', () => {
-        const parameter = new Parameter(goodAPIData, new ParameterSymbols(badSymbolsGetter));
+        const parameter = new Parameter(goodAPIData, new ParameterInfo(badSymbolsGetter), new ParameterInfo(goodSymbolsGetter));
 
         expect(console.log).toBeCalledTimes(1);
 
@@ -101,7 +101,7 @@ describe('Unit tests for rendering a parameter', () => {
     })
 
     test('Parameter class renders when correctly initialised', () => {
-        const parameter = new Parameter(goodAPIData, new ParameterSymbols(goodSymbolsGetter));
+        const parameter = new Parameter(goodAPIData, new ParameterInfo(goodSymbolsGetter), new ParameterInfo(goodSymbolsGetter));
 
         const result = parameter.html();
 
